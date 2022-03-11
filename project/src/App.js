5231f0a1682985237aec5f0a9fe759c33d755810
Scott Milliorn <scottmilliorn@gmail.com>
2022-03-10 19:30:22 -0800

    app div picture img

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
-
+import "./App.css";
+import portrait from "./images/portrait.jpg";
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="flex flex-wrap justify-center">
+      <picture className="w-1/2 px-4">
+        <img
+          src={portrait}
+          alt="..."
+          className="h-auto max-w-full align-middle border-none rounded shadow-lg"
+        />
+      </picture>
+    </div>
+  );
 }
 
 export default App;
